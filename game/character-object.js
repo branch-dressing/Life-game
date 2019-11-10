76122129258a2e2ce38c9d274ff387961d108ea2
joelpdurham <joeldur@gmail.com>
2019-11-09 21:22:54 -0800

    initial commit. plus has character object

---
+++ game/character-object.js
@@ -0,0 +1,27 @@
+const character = {
+    name: '',
+    happieness: 100,
+    age: 0,
+    sanity: 100,
+    expriences: {
+        positiveEx: {
+            childhood: '',
+            teenage: '',
+            earlyAdult: '',
+            adult: '',
+        },
+        negativeEx: {
+            childhood: '',
+            teenage: '',
+            earlyAdult: '',
+            adult: '',
+        },
+    },
+    favorites: {
+        number: 0,
+        word: '',
+        memory: '',
+    }
+}
+
+export default character;
