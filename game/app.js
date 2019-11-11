98c5181088d09aa0fc4e451980726c09550f60ab
joelpdurham <joeldur@gmail.com>
2019-11-10 18:57:29 -0800

    made some progress mostly on css, some functionality

--- game/app.js
+++ game/app.js
@@ -1,14 +1,25 @@
 import { countdownTimer } from './clock.js';
 import { postText } from './interactivity.js';
 
+const timeStamp = new Date().getTime();
+const endTime = timeStamp + 1200000;
+
+let timerText = document.getElementById('timer');
+//timerText.textContent ===
+
 //purposefully not called at the moment
-countdownTimer;
+//countdownTimer(endTime);
 
+
+
+//Text input
 const creatorInput = document.getElementById('creator-input');
-
 creatorInput.addEventListener('submit', function(event){
     event.preventDefault();
-    const text = document.getElementById('input-box').value;
     
-    postText('creator', text);
-})
+    let text = document.getElementById('input-box');
+    postText('creator', text.value);
+    text.value = '';
+});
+
+
--- game/clock.js
+++ game/clock.js
@@ -1,14 +1,15 @@
 import { runEndGame } from './end-game.js';
 
-export function countdownTimer() {
-    const endTime = new Date().getTime() + 1200000;
+//Sets a time for 20 mins from now, and displays the countdown on a second by second basis.
+//Else it ends the game.
+export function countdownTimer(endTime) {
     setInterval(function() {
         let now = new Date().getTime();
         let remainingTime = endTime - now;
 
         if (remainingTime >= 0) {
             let mins = Math.floor((remainingTime % 3600000) / 60000);
-            let secs = Math.floor((remainingTime % 60000) / 1000); 
+            let secs = Math.floor((remainingTime % 60000) / 1000);
             let timerText = document.getElementById('timer');
             timerText.textContent = `${mins}:${secs}`;
         } else {
